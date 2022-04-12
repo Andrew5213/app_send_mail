3d18e88c0acf1cc6ba4cb6c9de6ee1e98ca5b03f
andrew <andrewoliveira282@gmail>
2022-04-12 19:01:01 -0300

    Initial commit

---
+++ comentarios.php
@@ -0,0 +1,19 @@
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+//este e o comentario de uma unica linha
+#este tambem e um comentario de uma unica linha
+/*
+este sao comentarios de mais de uma linha 
+*/
+</body>
+
+</html>
+
+
---
+++ echo_print.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+<?php
+echo 'comando echo<br/>';
+print 'comando print<hr/>';
+?>
+</body>
+
+</html>
+
