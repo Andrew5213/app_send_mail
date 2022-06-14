98e7f2e2f4affa5d9a355fe2cb28dd1c5edffa6a
andrew <andrewoliveira282@gmail>
2022-06-14 14:27:35 -0300

    pesquisando arrays em php

--- pesquisa.php
+++ pesquisa.php
@@ -7,9 +7,27 @@
 
 <body>
 <?php
-
-
-
+#para fazer pesquisas dentro de array temos duas ferramentas nativas que e o
+//in_array(); retorna true ou false para a existencia
+//array_search(); ja este metodo retorna o indice do valor pesquisado caso ele exista
+$lista_frutas=['banana','maca','morango','uva'];
+echo '<pre>';
+print_r($lista_frutas);
+echo '</pre>';
+//agora vamos pesquisar a maca  utilizando o in_array
+$existe= in_array('maca',$lista_frutas);
+if ($existe){ // valores booleanos nao precisa colocar o == true na frente
+    echo "existe";
+}else{
+    echo "nao existe";
+}
+echo '<br>';
+$existe2= array_search('morango',$lista_frutas);
+if ($existe2==true){
+    echo 'existe na posicao:  '.$existe2;
+}else{
+    echo "nao existe";
+}
 ?>
 
 
