05df2126e158c051a6addc608ddd45da5eb57c42
andrew <andrewoliveira282@gmail>
2022-07-01 14:34:48 -0300

    autenticando o login

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -1,51 +1,60 @@
 <html>
-  <head>
-    <meta charset="utf-8" />
+<head>
+    <meta charset="utf-8"/>
     <title>App Help Desk</title>
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
-      .card-login {
-        padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
-      }
+        .card-login {
+            padding: 30px 0 0 0;
+            width: 350px;
+            margin: 0 auto;
+        }
     </style>
-  </head>
+</head>
 
-  <body>
+<body>
 
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">
+<nav class="navbar navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
         <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         App Help Desk
-      </a>
-    </nav>
+    </a>
+</nav>
 
-    <div class="container">    
-      <div class="row">
+<div class="container">
+    <div class="row">
 
         <div class="card-login">
-          <div class="card">
-            <div class="card-header">
-              Login
-            </div>
-            <div class="card-body">
-              <form action="valida_login.php" method="post"><!-aqui estamos passando onde sera procesado as acoes do form , alem disso deixamos o metodo post para nao mostar email e senha -->
+            <div class="card">
+                <div class="card-header">
+                    Login
+                </div>
+                <div class="card-body">
+                    <form action="valida_login.php" method="post"><!-aqui estamos passando onde sera procesado as acoes
+                        do form , alem disso deixamos o metodo post para nao mostar email e senha -->
 
-                <div class="form-group">
-                    <!-este name e para passar o valor contido no campo para o valida_login.php -->
-                  <input name="email"  type="email" class="form-control" placeholder="E-mail">
+                        <div class="form-group">
+                            <!-este name e para passar o valor contido no campo para o valida_login.php -->
+                            <input name="email" type="email" class="form-control" placeholder="E-mail">
+                        </div>
+                        <div class="form-group">
+                            <input name="senha" type="password" class="form-control" placeholder="Senha">
+                            <?php
+                            if (isset($_GET['login']) && $_GET['login'] == 'erro') {
+                                ?>
+                                <div class="text-danger">
+                                    Usuarios ou senha invalidos
+                                </div>
+                                <?php } ?>
+                        </div>
+                        <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+                    </form>
                 </div>
-                <div class="form-group">
-                  <input name="senha" type="password" class="form-control" placeholder="Senha">
-                </div>
-                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
-              </form>
             </div>
-          </div>
         </div>
     </div>
-  </body>
+</body>
 </html>
--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -10,9 +10,33 @@
 //echo $_GET['senha'];
 //aqui vamos modificar para deixar mais seguro
 //vamos verificar o post agora
-print_r($_POST);
-echo '<br/>';
-echo $_POST['email'];
-echo '<br/>';
-echo $_POST['senha'];
+
+//print_r($_POST);
+//echo '<br/>';
+//echo $_POST['email'];
+//echo '<br/>';
+//echo $_POST['senha'];
+//variavel para verificar autenticação
+$usuario_autenticado = false;
+//como nao adquiri conhecimento em banco de dados vamos deixar o sistema em um arrar
+$usuarios_array = [
+    ['email' => 'andrewoliveira282@gmail.com', 'senha' => '1234'],
+    ['email' => 'andrewoliveira28@gmail.com', 'senha' => 'abcd']
+
+];
+//echo '<pre/>';
+//print_r($usuarios_array);
+//echo '<pre/>';//este aqui era so para ver o array
+foreach ($usuarios_array as $user) {
+
+    //vamos fazer uma verificação pra ver se oq esta no form é igual o app
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+        $usuario_autenticado = true;
+    }
+}
+if ($usuario_autenticado==true){
+    echo "usuario autenticado ";
+}else{
+    header('location: index.php?login=erro');//para recarregar a pagina
+}
 ?>
