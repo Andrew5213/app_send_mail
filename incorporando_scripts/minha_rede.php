fceb30ef09800af0beacbf286d495fd384347d54
andrew <andrewoliveira282@gmail>
2022-07-04 13:14:14 -0300

    aprendendo um pouco sobre o reaproveitamento de scripts em php com include e require

---
+++ incorporando_scripts/minha_rede.php
@@ -0,0 +1,4 @@
+<?php
+include_once ('menu.php');//aqui estamos reaproveitado o codigo que esta no menu.php
+?>
+CONTEUDO DA PAGINA MINHA REDE
---
+++ incorporando_scripts/vagas.php
@@ -0,0 +1,6 @@
+<?php
+include_once('menu.php');//aqui estamos reaproveitado o codigo que esta no menu.php
+//require=> este ja faz um fatal erro enquanto o include so da um warning
+// o include_once nao deixa ser implementado mais de uma vez
+?>
+CONTEUDO DA PAGINA VAGAS
